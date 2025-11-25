02a8cf5854b00f71c9eb10c9fb29325a387a94c6
pendarium <thomas.sorli21@gmail.com>
2025-11-25 10:51:19 +0100

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Docstring for python_variable_annotations.5-sum_list
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Docstring for sum_list
+
+    :param input_list: Description
+    :type input_list: List[float]
+    :return: Description
+    :rtype: float
+    """
+    return sum(input_list)
