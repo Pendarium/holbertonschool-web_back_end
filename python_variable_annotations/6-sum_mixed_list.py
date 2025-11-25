69e54ac5775972d7eb3bb280370b03854fb5a4c3
pendarium <thomas.sorli21@gmail.com>
2025-11-25 10:56:16 +0100

    updt task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -7,7 +7,7 @@ Docstring for python_variable_annotations.6-mxd_list
 from typing import List
 
 
-def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[float | int]) -> float:
     """
     Docstring for sum_mixed_list
 
