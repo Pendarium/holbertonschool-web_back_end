006bbf8053eb0ef33ce426bdf15ea7b63d49545a
pendarium <thomas.sorli21@gmail.com>
2025-11-25 09:44:16 +0100

    add files + task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Docstring for python_variable_annotations.0-add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Docstring for add
+
+    :param a: Description
+    :type a: float
+    :param b: Description
+    :type b: float
+    :return: Description
+    :rtype: float
+    """
+    return a + b
