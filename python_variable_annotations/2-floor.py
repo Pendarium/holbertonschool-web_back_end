887ecedcd2bd0d7490f35751290b3bcacef6c642
pendarium <thomas.sorli21@gmail.com>
2025-11-25 10:34:30 +0100

    task 2/3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Docstring for python_variable_annotations.2-floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Docstring for floor
+
+    :param n: Description
+    :type n: float
+    :return: Description
+    :rtype: int
+    """
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Docstring for python_variable_annotations.3-to_str
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    Docstring for to_str
+
+    :param n: Description
+    :type n: float
+    :return: Description
+    :rtype: str
+    """
+    return math.to_str(n)
