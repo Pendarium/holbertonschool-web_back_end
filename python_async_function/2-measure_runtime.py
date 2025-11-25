ddf07c7efaa42abfb1c5ebd17ff3e257af0f6c23
pendarium <thomas.sorli21@gmail.com>
2025-11-25 14:37:06 +0100

    docstring

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Docstring for python_async_function.2-measure_runtime
+"""
 
 import asyncio
 import time
@@ -6,6 +9,16 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """
+    Docstring for measure_time
+    
+    :param n: Description
+    :type n: int
+    :param max_delay: Description
+    :type max_delay: int
+    :return: Description
+    :rtype: float
+    """
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
