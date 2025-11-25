56d918b62e93dc6f0a333249922b521715031dc8
pendarium <thomas.sorli21@gmail.com>
2025-11-25 14:37:44 +0100

    pycode

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -11,7 +11,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """
     Docstring for measure_time
-    
+
     :param n: Description
     :type n: int
     :param max_delay: Description
