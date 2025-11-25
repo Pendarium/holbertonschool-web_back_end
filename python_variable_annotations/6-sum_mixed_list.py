5d9e108fe1f979aec33d02ced3ff0ba530c327e2
pendarium <thomas.sorli21@gmail.com>
2025-11-25 10:54:51 +0100

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Docstring for python_variable_annotations.6-mxd_list
+"""
+
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+    """
+    Docstring for sum_mixed_list
+
+    :param mxd_lst: Description
+    :type mxd_lst: List[float, int]
+    :return: Description
+    :rtype: float
+    """
+    return sum(mxd_lst)
