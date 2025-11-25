122c4099906d5eecbeceda8899223490dd5ec3bd
pendarium <thomas.sorli21@gmail.com>
2025-11-25 09:50:03 +0100

    task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+Docstring for python_variable_annotations.1-concat
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    Docstring for concat
+
+    :param a: Description
+    :type a: str
+    :param b: Description
+    :type b: str
+    :return: Description
+    :rtype: str
+    """
+    return a + b
