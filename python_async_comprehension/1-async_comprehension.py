ea9fa6314d0aa634fd1bad0a2d9e2d6a6bfdc805
pendarium <thomas.sorli21@gmail.com>
2025-11-27 15:56:30 +0100

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+# Import from previous task
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    Collects 10 random numbers using an async comprehension
+    over async_generator, then returns them.
+    """
+    return [i async for i in async_generator()]
