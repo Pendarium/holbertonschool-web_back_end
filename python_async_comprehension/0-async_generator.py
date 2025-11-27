8710b43c105910820f35fe485168bab81043d419
pendarium <thomas.sorli21@gmail.com>
2025-11-27 15:41:10 +0100

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 
+"""
+Docstring for python_async_comprehension.0-async_generator
+"""
+
+from typing import AsyncGenerator
+import asyncio
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Docstring for async_generator
+
+    :return: Description
+    :rtype: AsyncGenerator[float, None]
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
