e3dae187039a23c4907b7e2753ad5c9e9edb717b
pendarium <thomas.sorli21@gmail.com>
2025-12-01 10:39:58 +0100

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -25,5 +25,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
             corresponding to the given page and page size.
     """
     start = (page - 1) * page_size
-    end = page * page_size
+    end = page + page_size
     return (start, end)
