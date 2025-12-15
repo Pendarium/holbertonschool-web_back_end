14983129999ab8de207fdc0c7fa7d881f04cf9ac
pendarium <thomas.sorli21@gmail.com>
2025-12-15 10:20:34 +0100

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+function getListStudents() {
+    return [
+        {
+            id: 1,
+            firstname: 'Guillaume',
+            location: 'San Francisco',
+        },
+        {
+            id: '2',
+            firstname: 'James',
+            location: 'Colombia',
+        },
+        {
+            id: '5',
+            firstname: 'Serena',
+            location: 'San Francisco',
+        },
+    ];
+}
