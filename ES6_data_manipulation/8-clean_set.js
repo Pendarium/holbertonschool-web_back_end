b8c26cdffa764a4c3ef85cc9876c574ccb7c5e85
pendarium <thomas.sorli21@gmail.com>
2025-12-18 17:08:12 +0100

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,6 @@
 export default function cleanSet(set, startString) {
-  const result = [];
-
-  for (const value of set) {
-    if (typeof value === 'string' && value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
-    }
-  }
-
-  return result.join('-');
+  return [...set]
+    .filter(value => typeof value === 'string' && value.startsWith(startString))
+    .map(value => value.slice(startString.length))
+    .join('-');
 }
