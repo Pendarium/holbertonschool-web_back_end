acee5d9f1c37293843e4c9f354bd593746f8993b
pendarium <thomas.sorli21@gmail.com>
2026-04-20 12:01:23 +0200

    task 1 and 2

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that provides a pagination helper function.
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple containing the start and end index for pagination.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
---
+++ pagination/README.md
@@ -0,0 +1,13 @@
+# Pagination Project
+
+## Description
+Explain what pagination is and what the project does.
+
+## Requirements
+Python 3.9, Ubuntu 20.04, pycodestyle...
+
+## Files
+- 0-simple_helper_function.py: helper function
+
+## Usage
+Explain how to run the code
