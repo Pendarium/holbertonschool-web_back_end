28ee877f43a2b8952c7a6a5c31ea09879b28db22
pendarium <thomas.sorli21@gmail.com>
2026-04-28 17:38:33 +0200

    loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const result = []
-  for (let value of array) {
-    result.push(appendString + value);
+  for (const value of array) {
+    const index = array.indexOf(value);
+    array[index] = appendString + value;
   }
-  return result;
+  return array;
 }
