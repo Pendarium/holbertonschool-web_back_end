c83470d1496afe0b5ffeed0aa9f1a21fe2430596
pendarium <thomas.sorli21@gmail.com>
2026-04-28 17:52:14 +0200

    commentaire

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,13 @@
 export default function createReportObject(employeesList) {
   const fullObject = {
+    /*varrivable de l'objet*/
     allEmployees:  {...employeesList},
+    /*list des employes*/
     getNumberOfDepartments(employeesList) {
+      /*recupere le departement de l'employees*/
         return Object.keys(employeesList).length;
+        /*retourn l'employe en question*/
     }  }
     return fullObject;
+    /*retourn l'objet complet qui comporte la listes des employer son departement et l'employer ciblé*/
 }
