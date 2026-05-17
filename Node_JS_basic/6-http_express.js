e7c273616b3de00b6759f49007f3533d168a768b
pendarium <thomas.sorli21@gmail.com>
2026-05-17 05:42:05 +0200

    display in page

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-module.exports = app;
+app.listen(1245);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
