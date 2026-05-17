61185ace8a0f88bf64a1ad90fdf3c904216386da
pendarium <thomas.sorli21@gmail.com>
2026-05-17 05:24:12 +0200

    4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,16 @@
-const http = require('http');
+const { createServer } = require('node:http');
 
-// Créer le serveur HTTP
-const app = http.createServer((req, res) => {
-  // Définir le type de contenu en texte brut
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const hostname = '127.0.0.1';
+const port = '1245';
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-// Écouter sur le port 1245
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
-});
-
-// Exporter le serveur pour pouvoir l'utiliser ailleurs
 module.exports = app;
+
+app.listen(port, hostname, () => {
+  console.log('Coucou les roudoudoux !');
+});
