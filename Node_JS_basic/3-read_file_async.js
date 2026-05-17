2a1aebfc97461409f716fd2f1c0d853d1c2e4cf4
pendarium <thomas.sorli21@gmail.com>
2026-05-17 05:22:27 +0200

    fix

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,40 +2,35 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
+    fs.readFile(path, 'utf8', ((err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
+      let totalStudents = 0;
+      const results = {};
 
-      const lines = data
-        .trim()
-        .split('\n')
-        .filter((line) => line.trim() !== '');
+      const lines = data.split('\n'); // le fichier devient un tableau de ligne en se basant sur \n pour répartir les lignes
+      const filteredLines = lines.filter((line) => line.trim() !== ''); // retirer les lignes vides
+      const studentLines = filteredLines.slice(1);
+      // on lit le tableau de ligne à partir de l'index 1 (sans les entêtes)
 
-      const students = lines.slice(1);
-
-      let output = `Number of students: ${students.length}\n`;
-
-      const fields = {};
-
-      for (const student of students) {
-        const parts = student.split(',');
-        const firstname = parts[0];
-        const field = parts[parts.length - 1];
-
-        if (!fields[field]) {
-          fields[field] = [];
+      for (const line of studentLines) { // pour chaque ligne...
+        totalStudents += 1;
+        const content = line.split(','); // va faire un tableau en séparant les valeurs via la ','
+        if (!(content[3] in results)) {
+          results[content[3]] = { students_nb: 1, students_list: [content[0]] };
+        } else {
+          results[content[3]].students_nb += 1;
+          results[content[3]].students_list.push(content[0]);
         }
-        fields[field].push(firstname);
       }
-
-      for (const field of Object.keys(fields)) {
-        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [key, value] of Object.entries(results)) {
+        console.log(`Number of students in ${key}: ${value.students_nb}. List: ${value.students_list.join(', ')}`);
       }
-
-      resolve(output.trim());
-    });
+      resolve(results);
+    }));
   });
 }
 
